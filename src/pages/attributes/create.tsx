4e0c7504261cdc40f6e47445df52becdc7b7f279
Manh Linh <mlinhalone@gmail.com>
2025-05-22 16:22:54 +0700

    feat: Hiển thị danh sách thuộc tính

---
+++ src/pages/attributes/create.tsx
@@ -0,0 +1,68 @@
+import { Create, useForm } from "@refinedev/antd";
+import { Form, Input, Button, Space } from "antd";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+
+export const AttributeCreate = () => {
+  const { formProps, saveButtonProps } = useForm({});
+
+  return (
+    <Create title="Tạo thuộc tính" saveButtonProps={saveButtonProps}>
+      <Form {...formProps} layout="vertical">
+        <Form.Item
+          label="Tên thuộc tính"
+          name={["name"]}
+          rules={[{ required: true, message: "Vui lòng nhập tên thuộc tính" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.List
+          name={["values"]}
+          rules={[
+            {
+              validator: async (_, values) => {
+                if (!values || values.length < 1) {
+                  return Promise.reject(
+                    new Error("Vui lòng nhập ít nhất một giá trị")
+                  );
+                }
+              },
+            },
+          ]}
+        >
+          {(fields, { add, remove }) => (
+            <>
+              <label>Giá trị</label>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space
+                  key={key}
+                  style={{ display: "flex", marginBottom: 8 }}
+                  align="baseline"
+                >
+                  <Form.Item
+                    {...restField}
+                    name={name}
+                    rules={[{ required: true, message: "Không được để trống" }]}
+                  >
+                    <Input placeholder="Giá trị thuộc tính" />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => add()}
+                  block
+                  icon={<PlusOutlined />}
+                >
+                  Thêm giá trị
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+      </Form>
+    </Create>
+  );
+};
